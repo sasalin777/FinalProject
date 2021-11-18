a85b5e94c682f83727a2702c0c5ff59ecd55a16e
mau-io <maurice222@outlook.com>
2021-11-17 20:04:57 -0800

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.finalproject to javafx.fxml;
+    exports com.example.finalproject;
+}
